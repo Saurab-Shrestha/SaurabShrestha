9c3ab12636d5691a65b2fd01f59e807dd8a7ffbc
Saurab505 <saurab@quickfoxconsulting.com>
2025-09-30 20:38:49 +0545

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 import { appConfig } from './config/app.config';
 
 export default defineConfig({
-  base: '/my-portfolio/',
+  base: '',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
